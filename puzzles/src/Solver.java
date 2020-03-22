8c6061db7c5c2866304f940abe2439c70eb7793b
Leaf <openlb12@163.com>
2020-03-23 02:41:38 +0800

    week4 8-puzzle

--- puzzles/src/Solver.java
+++ puzzles/src/Solver.java
@@ -1,38 +1,141 @@
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
 
 public class Solver {
-    private MinPQ<Board> game_tree = new MinPQ<Board>();
 
-    private Stack<Board>
+
+    private boolean isSolvable;
+    private BoardNode solution;
 
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
         if (initial == null) {
             throw new IllegalArgumentException("Illegal initial board\n");
         }
-        game_tree.insert(initial);
-        while (!game_tree.min().isGoal()) {
-            Board stepBoard = game_tree.delMin();
-            for (Board item : stepBoard.neighbors()) {
-                if (!item.equals(initial)) {
-                    game_tree.insert(item);
+        MinPQ<BoardNode> search_node = new MinPQ<BoardNode>();
+        isSolvable = false;
+        solution = null;
+        search_node.insert(new BoardNode(null, initial));
+        while (!search_node.isEmpty()) {
+            BoardNode current = search_node.delMin();
+            if (current.getNode().isGoal()) {
+                isSolvable = true;
+                solution = current;
+                break;
+            }
+            for (Board it : current.getNode().neighbors()) {
+                BoardNode snode = new BoardNode(current, it);
+                BoardNode retriever = current.previous_node;
+
+
+                boolean addNode = true;
+
+                while (retriever != null) {
+                    if (snode.isEqual(retriever)) {
+                        addNode = false;
+                        break;
+                    }
+                    retriever = retriever.previous_node;
+                }
+                if (addNode) {
+                    search_node.insert(snode);
                 }
             }
+
         }
+
     }
 
 
     // is the initial board solvable? (see below)
-    public boolean isSolvable()
+    public boolean isSolvable() {
+        return isSolvable;
+    }
 
     // min number of moves to solve initial board
-    public int moves()
+    public int moves() {
+        return solution.getMoves();
+    }
 
     // sequence of boards in a shortest solution
-    public Iterable<Board> solution()
+    public Iterable<Board> solution() {
+        Stack<Board> solution_step = new Stack<Board>();
+        BoardNode step = solution;
+        while (step != null) {
+            solution_step.push(step.getNode());
+            step = step.previous_node;
+        }
+        return solution_step;
+    }
 
     // test client (see below)
-    public static void main(String[] args)
+    public static void main(String[] args) {
+
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] tiles = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                tiles[i][j] = in.readInt();
+        Board initial = new Board(tiles);
+
+        // solve the puzzle
+        Solver solver = new Solver(initial);
+
+        // print solution to standard output
+        if (!solver.isSolvable())
+            StdOut.println("No solution possible");
+        else {
+            StdOut.println("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution())
+                StdOut.println(board);
+        }
+    }
+
+    private class BoardNode implements Comparable<BoardNode> {
+        private Board node;
+        private int priority;
+        private int moves;
+        private BoardNode previous_node;
+
+        public BoardNode(BoardNode pn, Board bd) {
+            node = bd;
+            previous_node = pn;
+            if (pn == null) {
+                moves = 0;
+            } else {
+                moves = pn.getMoves() + 1;
+            }
+            priority = moves + node.manhattan();
+        }
+
+        @Override
+        public int compareTo(BoardNode o) {
+            return this.priority - o.getPriority();
+        }
+
+        public int getPriority() {
+            return priority;
+        }
+
+        public int getMoves() {
+            return moves;
+        }
+
+        public Board getNode() {
+            return node;
+        }
+
+        public BoardNode getPrevious_node() {
+            return previous_node;
+        }
+
+        public boolean isEqual(BoardNode x) {
+            return this.node.equals(x.getNode());
+        }
+    }
 
 }
