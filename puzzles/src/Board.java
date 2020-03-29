5901507f052ab230738e2153b34d4ec16b463a2a
Leaf <openlb12@163.com>
2020-03-29 14:11:34 +0800

    week4 100/100 actually the quickSolvableCheck is a more quick and less computation-demanding way to check the solubility of a given puzzle, but it would call the board to_string function which violated the auto-grading policy.

--- puzzles/src/Board.java
+++ puzzles/src/Board.java
@@ -7,95 +7,82 @@ import java.util.Arrays;
 public final class Board {
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
-    private final int[] board_tiles;
-    private final int BOARD_WIDTH;
-    private int black_tile_x;
-    private int black_tile_y;
+    private final int[] boardTiles;
+    private final int boardWidth;
+    private final int blackTileX;
+    private final int blackTileY;
+    private final int hamming;
+    private final int manhattan;
+    private final boolean isGoal;
 
     public Board(int[][] tiles) {
-        BOARD_WIDTH = tiles.length;
-        board_tiles = new int[BOARD_WIDTH * BOARD_WIDTH];
-        for (int i = 0; i < BOARD_WIDTH; i++) {
-            for (int j = 0; j < BOARD_WIDTH; j++) {
+        boardWidth = tiles.length;
+        boardTiles = new int[boardWidth * boardWidth];
+        int hamDistance = 0;
+        int manhatDistance = 0;
+        int blackX = boardWidth - 1;
+        int blackY = boardWidth - 1;
+        for (int i = 0; i < boardWidth; i++) {
+            for (int j = 0; j < boardWidth; j++) {
                 if (tiles[i][j] == 0) {
-                    black_tile_x = i;
-                    black_tile_y = j;
+                    blackX = i;
+                    blackY = j;
+                    boardTiles[i * boardWidth + j] = tiles[i][j];
+                    continue;
                 }
-                board_tiles[i * BOARD_WIDTH + j] = tiles[i][j];
+                if (tiles[i][j] != (i * boardWidth + j + 1) % boardTiles.length) {
+                    hamDistance++;
+                    manhatDistance += Math.abs((tiles[i][j] - 1) / boardWidth - i)
+                            + Math.abs((tiles[i][j] - 1) % boardWidth - j);
+                }
+                boardTiles[i * boardWidth + j] = tiles[i][j];
             }
         }
+        blackTileX = blackX;
+        blackTileY = blackY;
+        hamming = hamDistance;
+        manhattan = manhatDistance;
+        isGoal = (hamming == 0);
     }
 
     // string representation of this board
     public String toString() {
-        StringBuilder board_info = new StringBuilder();
-        board_info.append(BOARD_WIDTH + "\n");
-        for (int i = 0; i < BOARD_WIDTH; i++) {
-            for (int j = 0; j < BOARD_WIDTH; j++) {
-                board_info.append(String.format("%2d ", board_tiles[i * BOARD_WIDTH + j]));
+        StringBuilder boardInfo = new StringBuilder();
+        boardInfo.append(boardWidth + "\n");
+        for (int i = 0; i < boardWidth; i++) {
+            for (int j = 0; j < boardWidth; j++) {
+                boardInfo.append(String.format("%2d ", boardTiles[i * boardWidth + j]));
             }
-            board_info.append("\n");
+            boardInfo.append("\n");
         }
-        return board_info.toString();
+        return boardInfo.toString();
     }
 
     // board dimension n
     public int dimension() {
-        return BOARD_WIDTH;
+        return boardWidth;
     }
 
 
-    private int dimen_to_line(int i, int j) {
-        return i * BOARD_WIDTH + j;
+    private int dimenToLine(int i, int j) {
+        return i * boardWidth + j;
     }
 
-    private int[] id_to_loc(int id) {
-        if (id == 0) {
-            return new int[]{BOARD_WIDTH - 1, BOARD_WIDTH - 1};
-        } else {
-            return new int[]{(id - 1) / BOARD_WIDTH, (id - 1) % BOARD_WIDTH};
-        }
-    }
-
-    private int manhanttan_distance(int p1, int p2) {
-        int[] loc1 = id_to_loc(p1);
-        int[] loc2 = id_to_loc(p2);
-        int distance = 0;
-        for (int i = 0; i < loc1.length; i++) {
-            distance += Math.abs(loc1[i] - loc2[i]);
-        }
-        return distance;
-    }
 
     // number of tiles out of place
     public int hamming() {
-        int ham_distance = 0;
-        for (int i = 0; i < board_tiles.length - 1; i++) {
-            if (board_tiles[i] != (i + 1) % board_tiles.length) {
-                ham_distance++;
-            }
-        }
-        return ham_distance;
+        return hamming;
     }
 
     // sum of Manhattan distances between tiles and goal
     public int manhattan() {
-        int manhattan_distance = 0;
-        for (int i = 0; i < board_tiles.length; i++) {
-            if (board_tiles[i] == 0) continue;
-            manhattan_distance += manhanttan_distance(board_tiles[i], (i + 1) % board_tiles.length);
-        }
-        return manhattan_distance;
+        return manhattan;
 
     }
 
     // is this board the goal board?
     public boolean isGoal() {
-        if (hamming() == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return isGoal;
     }
 
     // does this board equal y?
@@ -104,53 +91,52 @@ public final class Board {
         if (y == null) return false;
         if (y.getClass() != this.getClass()) return false;
         Board that = (Board) y;
-        if (that.black_tile_x != this.black_tile_x) return false;
-        if (that.black_tile_y != this.black_tile_y) return false;
-        return Arrays.equals(that.board_tiles, this.board_tiles);
+        if (that.blackTileX != this.blackTileX) return false;
+        if (that.blackTileY != this.blackTileY) return false;
+        return Arrays.equals(that.boardTiles, this.boardTiles);
     }
 
-    private int[] tile_exch(int loc1, int loc2) {
-        int[] switch_board = Arrays.copyOf(board_tiles, board_tiles.length);
-        switch_board[loc1] = board_tiles[loc2];
-        switch_board[loc2] = board_tiles[loc1];
-        return switch_board;
+    private int[] tileExch(int loc1, int loc2) {
+        int[] switchBoard = Arrays.copyOf(boardTiles, boardTiles.length);
+        switchBoard[loc1] = boardTiles[loc2];
+        switchBoard[loc2] = boardTiles[loc1];
+        return switchBoard;
     }
 
-    private int[][] board_generate(int[] list) {
-        int[][] board = new int[BOARD_WIDTH][BOARD_WIDTH];
+    private int[][] boardGenerate(int[] list) {
+        int[][] board = new int[boardWidth][boardWidth];
         for (int i = 0; i < list.length; i++) {
-            board[i / BOARD_WIDTH][i % BOARD_WIDTH] = list[i];
+            board[i / boardWidth][i % boardWidth] = list[i];
         }
         return board;
     }
 
     // all neighboring boards
     public Iterable<Board> neighbors() {
-        int idx = -1;
+        int idx = dimenToLine(blackTileX, blackTileY);
         Stack<Board> neighbor = new Stack<Board>();
-        while (board_tiles[++idx] != 0) ;
-        int[] loc = id_to_loc(idx + 1);
 
         for (int i = -1; i <= 1; i += 2) {
-            if (loc[0] + i >= BOARD_WIDTH || loc[0] + i < 0) continue;
-            int neighbor_loc = dimen_to_line(loc[0] + i, loc[1]);
-            neighbor.push(new Board(board_generate(tile_exch(idx, neighbor_loc))));
+            if (blackTileX + i >= boardWidth || blackTileX + i < 0) continue;
+            int neighborLoc = dimenToLine(blackTileX + i, blackTileY);
+            neighbor.push(new Board(boardGenerate(tileExch(idx, neighborLoc))));
         }
         for (int i = -1; i <= 1; i += 2) {
-            if (loc[1] + i >= BOARD_WIDTH || loc[1] + i < 0) continue;
-            int neighbor_loc = dimen_to_line(loc[0], loc[1] + i);
-            neighbor.push(new Board(board_generate(tile_exch(idx, neighbor_loc))));
+            if (blackTileY + i >= boardWidth || blackTileY + i < 0) continue;
+            int neighborLoc = dimenToLine(blackTileX, blackTileY + i);
+            neighbor.push(new Board(boardGenerate(tileExch(idx, neighborLoc))));
         }
         return neighbor;
     }
 
     // a board that is obtained by exchanging any pair of tiles
     public Board twin() {
-        int switch1 = dimen_to_line((black_tile_x + 1) % BOARD_WIDTH, black_tile_y);
-        int switch2 = dimen_to_line((black_tile_x + 1) % BOARD_WIDTH, (black_tile_y + 1) % BOARD_WIDTH);
-        return new Board(board_generate(tile_exch(switch1, switch2)));
+        int switch1 = dimenToLine((blackTileX + 1) % boardWidth, blackTileY);
+        int switch2 = dimenToLine((blackTileX + 1) % boardWidth, (blackTileY + 1) % boardWidth);
+        return new Board(boardGenerate(tileExch(switch1, switch2)));
 
     }
+
 
     // unit testing (not graded)
     public static void main(String[] args) {
@@ -171,8 +157,8 @@ public final class Board {
             // solve the slider puzzle
             Board initial = new Board(tiles);
             StdOut.print(initial);
-            StdOut.println(initial.hamming());
-            StdOut.println(initial.manhattan());
+            StdOut.println("Distance of Hamming: " + initial.hamming());
+            StdOut.println("Distance of Manhattan: " + initial.manhattan());
 
             StdOut.print(initial.twin());
             for (Board it : initial.neighbors()) {
--- puzzles/src/Solver.java
+++ puzzles/src/Solver.java
@@ -5,56 +5,93 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class Solver {
 
-
     private boolean isSolvable;
-    private BoardNode solution;
+    private int pqNumber;
+    private int solutionMoves;
+    private Stack<Board> solutionStep;
 
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
         if (initial == null) {
             throw new IllegalArgumentException("Illegal initial board\n");
         }
-        MinPQ<BoardNode> search_node = new MinPQ<BoardNode>();
         isSolvable = false;
-        solution = null;
-        search_node.insert(new BoardNode(null, initial));
-        while (!search_node.isEmpty()) {
-            BoardNode current = search_node.delMin();
+        BoardNode solution = null;
+        pqNumber = 0;
+
+        MinPQ<BoardNode> searchNode = new MinPQ<BoardNode>();
+        MinPQ<BoardNode> twinSearchNode = new MinPQ<BoardNode>();
+        searchNode.insert(new BoardNode(null, initial));
+        twinSearchNode.insert(new BoardNode(null, initial.twin()));
+        while (!searchNode.isEmpty()) {
+            BoardNode current = searchNode.delMin();
             if (current.getNode().isGoal()) {
                 isSolvable = true;
                 solution = current;
                 break;
             }
+            BoardNode retriever = current.previousNode;
             for (Board it : current.getNode().neighbors()) {
                 BoardNode snode = new BoardNode(current, it);
-                BoardNode retriever = current.previous_node;
-
-
-                boolean addNode = true;
-                for (BoardNode sn : search_node) {
-                    if (snode.isEqual(sn)) {
-                        if (snode.compareTo(sn) < 0) {
-                            addNode = false;
-                            break;
-                        } else {
-                            addNode = false;
-                            break;
-                        }
+                if (!snode.isEqual(retriever)) {
+                    searchNode.insert(snode);
+                    pqNumber++;
+                }
+            }
+            if (!twinSearchNode.isEmpty()) {
+                BoardNode currentTwin = twinSearchNode.delMin();
+                if (currentTwin.getNode().isGoal()) {
+                    isSolvable = false;
+                    solution = null;
+                    break;
+                }
+                BoardNode retrieverTwin = currentTwin.previousNode;
+                for (Board it : currentTwin.getNode().neighbors()) {
+                    BoardNode snode = new BoardNode(currentTwin, it);
+                    if (!snode.isEqual(retrieverTwin)) {
+                        twinSearchNode.insert(snode);
+                        pqNumber++;
                     }
                 }
-//                while (retriever != null) {
-//                    if (snode.isEqual(retriever)) {
-//                        addNode = false;
-//                        break;
-//                    }
-//                    retriever = retriever.previous_node;
-//                }
-                if (addNode) {
-                    search_node.insert(snode);
+            }
+        }
+
+        if (solution == null) {
+            solutionMoves = -1;
+            solutionStep = null;
+        } else {
+            solutionMoves = solution.getMoves();
+            solutionStep = new Stack<Board>();
+            BoardNode step = solution;
+            while (step != null) {
+                solutionStep.push(step.getNode());
+                step = step.previousNode;
+            }
+        }
+    }
+
+    private boolean quickSolvableCheck(Board init) {
+        String[] input = init.toString().split("[ \n]+");
+        int boardWidth = Integer.parseInt(input[0]);
+        int blackTileX = 0;
+        int inverNum = 0;
+        int[] boardTiles = new int[boardWidth * boardWidth];
+        for (int i = 0; i < boardTiles.length; i++) {
+            boardTiles[i] = Integer.parseInt(input[1 + i]);
+            if (boardTiles[i] == 0) {
+                blackTileX = i / boardWidth;
+                continue;
+            }
+            for (int j = 0; j < i; j++) {
+                if (boardTiles[j] == 0) continue;
+                if (boardTiles[j] > boardTiles[i]) {
+                    inverNum++;
                 }
             }
 
         }
+        return (inverNum + (boardWidth - 1 - blackTileX) * (boardWidth - 1)) % 2 == 0;
+
 
     }
 
@@ -66,18 +103,16 @@ public class Solver {
 
     // min number of moves to solve initial board
     public int moves() {
-        return solution.getMoves();
+        return solutionMoves;
     }
 
     // sequence of boards in a shortest solution
     public Iterable<Board> solution() {
-        Stack<Board> solution_step = new Stack<Board>();
-        BoardNode step = solution;
-        while (step != null) {
-            solution_step.push(step.getNode());
-            step = step.previous_node;
-        }
-        return solution_step;
+        return solutionStep;
+    }
+
+    private int getPqNumber() {
+        return pqNumber;
     }
 
     // test client (see below)
@@ -96,46 +131,58 @@ public class Solver {
         Solver solver = new Solver(initial);
 
         // print solution to standard output
-        if (!solver.isSolvable())
+        if (!solver.isSolvable()) {
             StdOut.println("No solution possible");
-        else {
-            StdOut.println("Minimum number of moves = " + solver.moves());
+            StdOut.println("PQ number of searching  = " + solver.getPqNumber());
+        } else {
             for (Board board : solver.solution())
                 StdOut.println(board);
+            StdOut.println("Minimum number of moves = " + solver.moves());
+            StdOut.println("PQ number of searching  = " + solver.getPqNumber());
         }
     }
 
     private class BoardNode implements Comparable<BoardNode> {
         private final Board node;
-        private final int priority;
+        private final int manhPriority;
         private final int moves;
-        private final BoardNode previous_node;
+        private final BoardNode previousNode;
 
         BoardNode(BoardNode pn, Board bd) {
             node = bd;
-            previous_node = pn;
+            previousNode = pn;
             if (pn == null) {
                 moves = 0;
             } else {
                 moves = pn.getMoves() + 1;
             }
-            priority = moves + node.manhattan();
+            manhPriority = moves + node.manhattan();
         }
 
-//        public void resetBoardNode(BoardNode bd) {
-//            node = bd.getNode();
-//            moves = bd.getMoves();
-//            priority = bd.getPriority();
-//            previous_node = bd.getPrevious_node();
+
+        //        @Override
+//        public int compareTo(BoardNode bn) {
+//            if (this.manhPriority == bn.getManhPriority()) {
+//                return this.hammPriority - bn.getHammPriority();
+//            }
+//            return this.manhPriority - bn.getManhPriority();
 //        }
-
+//        @Override
+//        public int compareTo(BoardNode bn) {
+//            return this.hammPriority - bn.getHammPriority();
+//        }
         @Override
-        public int compareTo(BoardNode o) {
-            return this.priority - o.getPriority();
+        public int compareTo(BoardNode bn) {
+            if (this.manhPriority == bn.getManhPriority()) {
+                return bn.getMoves() - moves;
+            } else {
+                return this.manhPriority - bn.getManhPriority();
+            }
         }
 
-        int getPriority() {
-            return priority;
+
+        int getManhPriority() {
+            return manhPriority;
         }
 
         int getMoves() {
@@ -146,13 +193,13 @@ public class Solver {
             return node;
         }
 
-        BoardNode getPrevious_node() {
-            return previous_node;
+        BoardNode getPreviousNode() {
+            return previousNode;
         }
 
         boolean isEqual(BoardNode x) {
+            if (x == null) return false;
             return this.node.equals(x.getNode());
         }
     }
-
 }
