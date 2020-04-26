a215b1fdeebc709671f0db9b5cb866f911349c51
openlb12 <openlb12@163.com>
2020-04-26 22:35:47 +0800

    seancarver 100/100

--- seam/src/SeamCarver.java
+++ seam/src/SeamCarver.java
@@ -10,13 +10,16 @@ public class SeamCarver {
     private int width;
     private int height;
     private double[][] energy;
+    private int traceSize;
 
 
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
+        if (picture == null) throw new IllegalArgumentException();
         pic = new Picture(picture);
         width = pic.width();
         height = pic.height();
+        traceSize = width * height + 2;
         energy = new double[width][];
         for (int w = 0; w < width; w++) {
             energy[w] = new double[height];
@@ -29,7 +32,7 @@ public class SeamCarver {
 
     // current picture
     public Picture picture() {
-        return pic;
+        return new Picture(pic);
     }
 
     // width of current picture
@@ -77,131 +80,96 @@ public class SeamCarver {
         return energy[x][y];
     }
 
-    private Iterable<Integer> neighbours_h(int p) {
+    private Iterable<Integer> neighbours_h(int x, int y) {
         Stack<Integer> neighbours = new Stack<Integer>();
-        int y = (p - 1) / width;
-        int x = (p - 1) % width;
-        if (p == 0) {
-            for (int h = 0; h < height; h++) {
-                neighbours.push(h * width + 1);
-            }
-        } else if (p == width * height + 1) {
-            return neighbours;
-        } else {
+        int p = x + 1 + y * width;
+        if (height > 1) {
             if (x == width - 1) {
-                neighbours.push(width * height + 1);
-            } else if (isBoundaryY(y)) {
-                if (y == 0) {
-                    neighbours.push(p + 1);
-                    neighbours.push(p + 1 + width);
-                } else {
-                    neighbours.push(p + 1);
-                    neighbours.push(p + 1 - width);
-                }
+                neighbours.push(traceSize - 1);
+            } else if (y == 0) {
+                neighbours.push(p + 1);
+                neighbours.push(p + 1 + width);
+            } else if (y == height - 1) {
+                neighbours.push(p + 1 - width);
+                neighbours.push(p + 1);
             } else {
                 neighbours.push(p + 1 - width);
                 neighbours.push(p + 1);
                 neighbours.push(p + 1 + width);
             }
-
+        } else {
+            neighbours.push(p + 1);
         }
         return neighbours;
     }
 
-    private void dfs_h(Stack<Integer> top, boolean[] marked, int p) {
-        marked[p] = true;
-        for (int np : neighbours_h(p)) {
-            if (!marked[np]) dfs_h(top, marked, np);
-        }
-        top.push(p);
-    }
 
-    private Iterable<Integer> topologcial_h() {
-        Stack<Integer> top = new Stack<Integer>();
-        boolean[] marked = new boolean[width * height + 2];
-        dfs_h(top, marked, 0);
-        return top;
-    }
-
-    private Iterable<Integer> neighbours_v(int p) {
+    private Iterable<Integer> neighbours_v(int x, int y) {
         Stack<Integer> neighbours = new Stack<Integer>();
-        int y = (p - 1) / width;
-        int x = (p - 1) % width;
-        if (p == 0) {
-            for (int h = 0; h < width; h++) {
-                neighbours.push(h + 1);
-            }
-        } else if (p == width * height + 1) {
-            return neighbours;
-        } else {
+        int p = x + 1 + y * width;
+
+        if (width > 1) {
             if (y == height - 1) {
-                neighbours.push(width * height + 1);
-            } else if (isBoundaryX(x)) {
-                if (x == 0) {
-                    neighbours.push(p + width);
-                    neighbours.push(p + 1 + width);
-                } else {
-                    neighbours.push(p + width);
-                    neighbours.push(p - 1 + width);
-                }
-            } else {
-                neighbours.push(p - 1 + width);
+                neighbours.push(traceSize - 1);
+            } else if (x == 0) {
                 neighbours.push(p + width);
                 neighbours.push(p + 1 + width);
+            } else if (x == width - 1) {
+                neighbours.push(p + width);
+                neighbours.push(p - 1 + width);
+            } else {
+                neighbours.push(p + width);
+                neighbours.push(p - 1 + width);
+                neighbours.push(p + 1 + width);
             }
+        } else {
+            neighbours.push(p + width);
         }
+
         return neighbours;
-    }
-
-    private void dfs_v(Stack<Integer> top, boolean[] marked, int p) {
-        marked[p] = true;
-
-        for (int np : neighbours_v(p)) {
-            if (!marked[np]) dfs_h(top, marked, np);
-        }
-        top.push(p);
-    }
-
-    private Iterable<Integer> topologcial_v() {
-        Stack<Integer> top = new Stack<Integer>();
-        boolean[] marked = new boolean[width * height + 2];
-        dfs_v(top, marked, 0);
-        return top;
     }
 
 
     // sequence of indices for horizontal seam
     public int[] findHorizontalSeam() {
         int[] seam = new int[width];
-        final int mapSize = width * height + 2;
-        int[] edgeFrom = new int[mapSize];
-        double[] distanceTo = new double[mapSize];
-        for (int p = 0; p < mapSize; p++) {
+        int[] edgeFrom = new int[traceSize];
+        double[] distanceTo = new double[traceSize];
+        for (int p = 0; p < traceSize; p++) {
             distanceTo[p] = Double.POSITIVE_INFINITY;
         }
         edgeFrom[0] = 0;
         distanceTo[0] = 0.0;
-        for (int p : topologcial_h()) {
-            for (int np : neighbours_h(p)) {
-                try {
-                    int x = (np - 1) % width;
-                    int y = (np - 1) / width;
-                    if (distanceTo[np] > distanceTo[p] + energy[x][y]) {
-                        distanceTo[np] = distanceTo[p] + energy[x][y];
-                        edgeFrom[np] = p;
-                    }
-                } catch (IndexOutOfBoundsException e) {
-                    if (distanceTo[np] > distanceTo[p]) {
-                        distanceTo[np] = distanceTo[p];
-                        edgeFrom[np] = p;
+        for (int p = 0; p < height; p++) {
+            edgeFrom[p * width + 1] = 0;
+            distanceTo[p * width + 1] = energy(0, p);
+        }
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                int p = 1 + y * width + x;
+                for (int np : neighbours_h(x, y)) {
+                    try {
+                        int nx = (np - 1) % width;
+                        int ny = (np - 1) / width;
+                        if (distanceTo[np] > distanceTo[p] + energy[nx][ny]) {
+                            distanceTo[np] = distanceTo[p] + energy[nx][ny];
+                            edgeFrom[np] = p;
+                        }
+                    } catch (IndexOutOfBoundsException e) {
+                        if (distanceTo[np] > distanceTo[p]) {
+                            distanceTo[np] = distanceTo[p];
+                            edgeFrom[np] = p;
+                        }
                     }
                 }
             }
         }
 
-        int apex = edgeFrom[height * width + 1];
+
+        int apex = edgeFrom[traceSize - 1];
         for (int p = width - 1; p >= 0; p--) {
-            seam[p] = (edgeFrom[apex] - 1) / width;
+            seam[p] = (apex - 1) / width;
             apex = edgeFrom[apex];
         }
 
@@ -211,35 +179,42 @@ public class SeamCarver {
     // sequence of indices for vertical seam
     public int[] findVerticalSeam() {
         int[] seam = new int[height];
-        final int mapSize = width * height + 2;
-        int[] edgeFrom = new int[mapSize];
-        double[] distanceTo = new double[mapSize];
-        for (int p = 0; p < mapSize; p++) {
+        int[] edgeFrom = new int[traceSize];
+        double[] distanceTo = new double[traceSize];
+        for (int p = 0; p < traceSize; p++) {
             distanceTo[p] = Double.POSITIVE_INFINITY;
         }
         edgeFrom[0] = 0;
         distanceTo[0] = 0.0;
-        for (int p : topologcial_v()) {
-            for (int np : neighbours_v(p)) {
-                try {
-                    int x = (np - 1) % width;
-                    int y = (np - 1) / width;
-                    if (distanceTo[np] > distanceTo[p] + energy[x][y]) {
-                        distanceTo[np] = distanceTo[p] + energy[x][y];
-                        edgeFrom[np] = p;
-                    }
-                } catch (IndexOutOfBoundsException e) {
-                    if (distanceTo[np] > distanceTo[p]) {
-                        distanceTo[np] = distanceTo[p];
-                        edgeFrom[np] = p;
+
+        for (int x = 0; x < width; x++) {
+            edgeFrom[x + 1] = 0;
+            distanceTo[x + 1] = energy(x, 0);
+        }
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int p = 1 + y * width + x;
+                for (int np : neighbours_v(x, y)) {
+                    try {
+                        int nx = (np - 1) % width;
+                        int ny = (np - 1) / width;
+                        if (distanceTo[np] > distanceTo[p] + energy[nx][ny]) {
+                            distanceTo[np] = distanceTo[p] + energy[nx][ny];
+                            edgeFrom[np] = p;
+                        }
+                    } catch (IndexOutOfBoundsException e) {
+                        if (distanceTo[np] > distanceTo[p]) {
+                            distanceTo[np] = distanceTo[p];
+                            edgeFrom[np] = p;
+                        }
                     }
                 }
             }
         }
 
-        int apex = edgeFrom[height * width + 1];
+        int apex = edgeFrom[traceSize - 1];
         for (int p = height - 1; p >= 0; p--) {
-            seam[p] = (edgeFrom[apex] - 1) % width;
+            seam[p] = (apex - 1) % width;
             apex = edgeFrom[apex];
         }
 
@@ -248,12 +223,68 @@ public class SeamCarver {
 
     // remove horizontal seam from current picture
     public void removeHorizontalSeam(int[] seam) {
-        if (seam == null) throw new IllegalArgumentException();
+        boolean isSeamable = (seam != null) && (seam.length == width) && (height > 1);
+        if (!isSeamable) throw new IllegalArgumentException();
+        boolean isValiedSeam = isValidY(seam[0]);
+        for (int i = 1; i < width; i++) {
+            if (!isValidY(seam[i]) || Math.abs(seam[i] - seam[i - 1]) > 1) {
+                isValiedSeam = false;
+                break;
+            }
+        }
+        if (!isValiedSeam) throw new IllegalArgumentException();
+        height -= 1;
+        Picture rpic = new Picture(width, height);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < seam[x]; y++) {
+                rpic.setRGB(x, y, pic.getRGB(x, y));
+            }
+            for (int y = seam[x] + 1; y < pic.height(); y++) {
+                rpic.setRGB(x, y - 1, pic.getRGB(x, y));
+            }
+        }
+        pic = rpic;
+        traceSize = width * height + 2;
+        energy = new double[width][];
+        for (int w = 0; w < width; w++) {
+            energy[w] = new double[height];
+            for (int h = 0; h < height; h++) {
+                energy[w][h] = pix_energy(w, h);
+            }
+        }
     }
 
     // remove vertical seam from current picture
     public void removeVerticalSeam(int[] seam) {
-        if (seam == null) throw new IllegalArgumentException();
+        boolean isSeamable = (seam != null) && (seam.length == height) && (width > 1);
+        if (!isSeamable) throw new IllegalArgumentException();
+        boolean isValiedSeam = isValidX(seam[0]);
+        for (int i = 1; i < height; i++) {
+            if (!isValidX(seam[i]) || Math.abs(seam[i] - seam[i - 1]) > 1) {
+                isValiedSeam = false;
+                break;
+            }
+        }
+        if (!isValiedSeam) throw new IllegalArgumentException();
+        width -= 1;
+        Picture rpic = new Picture(width, height);
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < seam[y]; x++) {
+                rpic.setRGB(x, y, pic.getRGB(x, y));
+            }
+            for (int x = seam[y] + 1; x < pic.width(); x++) {
+                rpic.setRGB(x - 1, y, pic.getRGB(x, y));
+            }
+        }
+        pic = rpic;
+        traceSize = width * height + 2;
+        energy = new double[width][];
+        for (int w = 0; w < width; w++) {
+            energy[w] = new double[height];
+            for (int h = 0; h < height; h++) {
+                energy[w][h] = pix_energy(w, h);
+            }
+        }
     }
 
     //  unit testing (optional)
